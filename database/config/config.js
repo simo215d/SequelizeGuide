57fc8bee95743cf5f843e717104f1d13a7091ced
simo215d <simo215d@edu.easj.dk>
2020-11-05 11:16:50 +0100

    first commit

---
+++ database/config/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "secretbrah",
+    "database": "db1",
+    "host": "127.0.0.1",
+    "port": 32773,
+    "dialect": "mysql"
+  }
+}
